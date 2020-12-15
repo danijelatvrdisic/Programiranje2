2dbc04e46bd39ee7ba9688046677093164a54fe1
danijelatvrdisic <76062953+danijelatvrdisic@users.noreply.github.com>
2020-12-15 19:08:57 +0100

    Add files via upload

---
+++ dacat.py
@@ -0,0 +1,9 @@
+import turtle
+t=turtle.Turtle()
+t.penup()
+t.goto(0,200)
+t.pendown()
+t.color('black')
+style=('Coursier',15, 'italic')
+t.write('Profesore, molim Vas, dajte nam 10 bodova :)',font=style,align='center')
+turtle.done()
